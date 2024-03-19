c94bfcac6c66e6fd7b93bfbb900043b428de90e0
madosuki <yurikasamaaikatsu@gmail.com>
2024-03-19 11:25:05 +0900

    add a comments

--- src/live2d/scenes/renderer.ts
+++ src/live2d/scenes/renderer.ts
@@ -33,6 +33,7 @@ export class Live2dSceneRenderer {
       gl.flush();
 
       scene(live2dViewer);
+      // requestAnimationFrameはlong型な0以外の値なrequest idを返す． ref: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame#return_value
       this.requestAnimationFrameHandler = requestAnimationFrame(loop);
     };
 
