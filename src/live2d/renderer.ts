643573a90dd7697a44b02e8659e6d48c7d2d8256
madosuki <yurikasamaaikatsu@gmail.com>
2026-03-03 17:29:32 +0900

    temporary not use live2d/portrait.ts because dynamic import problem in requestAnimeationFrame

--- src/live2d/renderer.ts
+++ src/live2d/renderer.ts
@@ -77,11 +77,48 @@ export class Live2dSceneRenderer {
       }
       gl.flush();
 
-      scene(live2dViewer, isFailedLive2dLoadCore).catch((e) =>
-        window.backend.logError(e),
-      );
+      const { width, height } = live2dViewer.canvas;
+
+      const projection = live2dViewer.getNewMatrix44();
+
+      const key = live2dViewer.getCurrentModelKey();
+      const model = live2dViewer.getModelFromKey(key);
+      if (model == undefined) {
+        // window.backend.logError("target Live2D Model is undefined");
+        return;
+      }
+
+      const draw = () => {
+        if (model.getModel()) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+          if (model.getModel().getCanvasWidth() > 1.0 && width < height) {
+            model.getModelMatrix().setWidth(2.0);
+            projection.scale(1.0, width / height);
+          } else {
+            projection.scale(height / width, 1.0);
+          }
+
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          projection.multiplyByMatrix(live2dViewer._viewMatrix);
+
+          model.update();
+          model.draw(projection, 0, 0, width, height, live2dViewer.frameBuffer);
+        }
+      };
+
+      if (model != undefined && model.isCompleteSetup) {
+        draw();
+      }
+
       // requestAnimationFrameはunsigned long型な0以外の値なrequest idを返す． ref: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame#return_value
       this.requestAnimationFrameHandler = requestAnimationFrame(loop);
+
+      /*
+      scene(live2dViewer, isFailedLive2dLoadCore).then(() => {
+        // requestAnimationFrameはunsigned long型な0以外の値なrequest idを返す． ref: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame#return_value
+        this.requestAnimationFrameHandler = requestAnimationFrame(loop);
+      })
+      */
     };
 
     if (this.requestAnimationFrameHandler !== 0) {
